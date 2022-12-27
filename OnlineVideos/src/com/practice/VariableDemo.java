38e2146e91a859ba030466658a6e0622c189e1e0
lavanya-g5 <lavanyag.git@gmail.com>
2022-12-27 19:04:00 +0530

    Variable example

---
+++ OnlineVideos/src/com/practice/VariableDemo.java
@@ -0,0 +1,22 @@
+//Variable - To store the data we use variables, values are allowed to change
+// int i =5; initialization & declaration
+// here int -> data type, i -> variable name, 5 -> value
+package com.practice;
+
+public class VariableDemo {
+	
+	public static void main(String[] args) {
+		
+		int i =5;
+		System.out.println("Integer 1: "+i);
+		
+		//Assigning new value to variable i
+		i=6;
+		System.out.println("Integer 2: "+i);
+	}
+
+}
+
+
+//Integer 1: 5
+//Integer 2: 6
