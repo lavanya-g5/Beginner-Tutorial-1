09b439e730742d7652403e58fc8495b6b7d30e1e
lavanya-g5 <lavanyag.git@gmail.com>
2022-12-27 18:19:38 +0530

    First code - Hello World!!!

---
+++ OnlineVideos/src/FirstCode.java
@@ -0,0 +1,14 @@
+// keywords - public, class. static, void 
+// Class - FirstCode, String, System 
+// Note: String and System are predefined classes, not user defined 
+// Variables - out, args
+// method - main, println
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+}
