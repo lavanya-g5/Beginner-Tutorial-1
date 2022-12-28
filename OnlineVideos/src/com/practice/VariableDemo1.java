ca6294090a74a078084ee12924f634fbc8aa196e
lavanya-g5 <lavanyag.git@gmail.com>
2022-12-28 18:41:02 +0530

    Variable Demo 1 & Variable Naming Convention

---
+++ OnlineVideos/src/com/practice/VariableDemo1.java
@@ -0,0 +1,28 @@
+// Variable - Naming convention
+// Interface - Name should be an Adjective (Eg: Runnable, Comparable) Note - Not mandatory to have Able at the interface name
+// Class Name - Should be Nouns (Eg: Student, Employee, Person)
+// For Class & Interface - First letter should be capital
+// Method - Should be Verbs (Eg: Printing, Barking, Displaying)
+// Constant - we need to use everything in CAPITAL - values should be in Upper case (When you want fixed value Eg: PI, DENSITY, MAX_PRICE)
+// For variable & method - First letter should be in lower case
+// Class name - If you have more than one word, follow camel case naming convention (eg: VariableDemo, EmployeeDetails)
+// Method & variable - more that one word, first letter in first word should be lower case, second word 1st letter should be upper case
+// Eg: actionPerformed, stockExchange
+// Advantage - Makes code More Readable, Efficient
+// Variable - abc
+// Constant - ABC
+// Contructor - Abc()
+// Method - abc()
+// Interface - Stretchable 
+
+package com.practice;
+
+public class VariableDemo1 {
+
+	public static void main(String[] args) {
+
+		int abc = 1; // Variable
+		System.out.println("Variable value is " + abc);
+	}
+
+}
