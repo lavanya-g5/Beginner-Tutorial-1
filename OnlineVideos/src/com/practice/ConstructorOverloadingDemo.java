5a1003aefdc4ed7618d7693a6112b5dc4becc456
lavanya-g5 <lavanyag.git@gmail.com>
2023-01-06 12:04:25 +0530

    Constructor overloading & Parameterized constructor example

--- OnlineVideos/src/com/practice/ConstructorOverloadingDemo.java
+++ OnlineVideos/src/com/practice/ConstructorOverloadingDemo.java
@@ -9,17 +9,18 @@ class ConstructorOverloading {
 	int i;
 	float f;
 
-	public ConstructorOverloading() {
+	// Constructor is called, when object is created
+	public ConstructorOverloading() { // without parameter
 		i = 4;
 		f = 2;
 		System.out.println("Calling Default constructor.. " + i + " & " + f);
 	}
 
-	public ConstructorOverloading(String value) {
+	public ConstructorOverloading(String value) { // with single parameter
 		System.out.println("Calling parameterized constructor.. " + value);
 	}
 
-	public ConstructorOverloading(int a, double b, String c) {
+	public ConstructorOverloading(int a, double b, String c) { // with 3 paramters
 		System.out.println("Value of a, b & c is: " + a + ", " + b + ", " + c);
 	}
 }
@@ -29,8 +30,13 @@ public class ConstructorOverloadingDemo {
 
 		ConstructorOverloading cv = new ConstructorOverloading(); // This will call the default constructor
 		// ConstructorOverloading()
+
+//		While creating the object,if you pass a value, it will call that particular
+		// constructor which takes string value
 		ConstructorOverloading cv1 = new ConstructorOverloading("ABCD"); // This will call the parameterized constructor
 		// ConstructorOverloading(String)
+
+//		While creating the object,if you pass a value, it will call that particular constructor which takes int,double and String value
 		ConstructorOverloading cv2 = new ConstructorOverloading(5, 9, "EXAMPLE"); // This will call the parameterized
 		// constructor ConstructorOverloading(int,double,String)
 	}
